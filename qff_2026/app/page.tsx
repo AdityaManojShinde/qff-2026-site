2aa55624ed6e716113602868da7f79f3e3d02f95
AdityaManojShinde <aditya.manoj.shinde@gmail.com>
2026-09-07 09:19:00 +0530

    added: add todo comments to main page.tsx

--- qff_2026/app/page.tsx
+++ qff_2026/app/page.tsx
@@ -2,7 +2,11 @@ import Hero from "@/components/sections/Hero";
 
 {/* add sections here, every section component should have a SectionContainer wrapper for consistent spacing and alignment */}
 
+// Todo: Add Hero Section -> done
 // Todo: Add About Section
+// Todo: Add Schedule section
+// Todo: Add Speakers Section
+// Todo: Add FAQ Section
 
 export default function Home() {
   return (
